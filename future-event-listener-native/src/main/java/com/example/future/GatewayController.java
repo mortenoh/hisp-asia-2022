89dd55d4102b0a5a35126dd9b3503ad53db9f1be
Morten Hansen <morten@winterop.com>
2023-02-22 15:28:10 +0700

    update

--- future-event-listener-native/src/main/java/com/example/future/GatewayController.java
+++ future-event-listener-native/src/main/java/com/example/future/GatewayController.java
@@ -22,13 +22,15 @@ public class GatewayController {
     private final ObjectMapper objectMapper;
 
     @PostMapping
-    public ResponseEntity<?> whRequest(@RequestBody Map<String, ?> payload, @RequestHeader("Authorization") String token) throws IOException {
+    public ResponseEntity<?> whRequest(@RequestBody Map<String, ?> payload, @RequestHeader("Authorization") String token) throws IOException, InterruptedException {
         if (!"ApiToken EB3F6799-AA5A-47E8-B6B7-97EA54EB3873".equals(token)) {
             throw new HttpClientErrorException(HttpStatusCode.valueOf(401));
         }
 
         System.err.println("-- Webhook Request Received, data:\n");
         System.err.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(payload));
+
+        Thread.sleep(2_000);
 
         return ResponseEntity.ok().build();
     }
